e02c832b0913174163bfb1c9b2da0af97b222d74
AppolinFotso <appolin.s.fotso@gmail.com>
2023-04-28 11:43:51 +0100

    Added for loop to render all the episodes
    I used a for loop to add each episode on the root element.

--- script.js
+++ script.js
@@ -6,11 +6,22 @@ function setup() {
 
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
-  const episodeContainer = document.createElement("div");
-  const episodeTitle = document.createElement("h2");
-  const episodePoster = document.createElement("img");
-  const episodeSummary = document.createElement("p");
+  for (let episode of episodeList) {
+    const episodeContainer = document.createElement("div");
+    const episodeTitle = document.createElement("h2");
+    const episodePoster = document.createElement("img");
+    const episodeSummary = document.createElement("p");
+
+    episodeTitle.textContent = `${episode.name}-${
+      episode.season < 10 ? `S0${episode.season}` : `S${episode.season}`
+    }${episode.number < 10 ? `E0${episode.number}` : `E${episode.number}`}`;
+    episodePoster.setAttribute("src", `${episode.image.medium}`);
+    episodeSummary.innerHTML = `${episode.summary}`;
+    episodeContainer.appendChild(episodeTitle);
+    episodeContainer.appendChild(episodePoster);
+    episodeContainer.appendChild(episodeSummary);
+    rootElem.appendChild(episodeContainer);
+  }
 }
 
 window.onload = setup;
