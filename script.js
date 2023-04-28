9e5a804b45fd813f1205dada82af0790e0d68cc2
AppolinFotso <appolin.s.fotso@gmail.com>
2023-04-28 11:04:28 +0100

    Created html elements
    Each element hold some information about a specific episode.

--- script.js
+++ script.js
@@ -7,6 +7,10 @@ function setup() {
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
   rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  const episodeContainer = document.createElement("div");
+  const episodeTitle = document.createElement("h2");
+  const episodePoster = document.createElement("img");
+  const episodeSummary = document.createElement("p");
 }
 
 window.onload = setup;
